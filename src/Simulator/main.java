d07a09c3f97aae51242fbaa8c109a73fca783598
ROMET <rometpierre@gmail.com>
2019-05-21 17:23:48 +0200

    simulator main class to debug

--- src/Simulator/main.java
+++ src/Simulator/main.java
@@ -2,23 +2,28 @@ package Simulator;
 
 import Graph.*;
 import Ant_Colony.*;
-import Shortest_Path.*;
+//import Shortest_Path.*;
 
+/**
+ * 
+ * @author promet
+ *
+ */
 public class main {
 
 	public static void main(String[] args) {
 		
-		//Creation of Vertices
-		Vertex vtx1 = new Vertex ("1");
-		Vertex vtx2 = new Vertex ("2");
-		Vertex vtx3 = new Vertex ("3");
-		Vertex vtx4 = new Vertex ("4");
-		Vertex vtx5 = new Vertex ("5");
-		Vertex vtx6 = new Vertex ("6");
-		Vertex vtx7 = new Vertex ("7");
-		Vertex vtx8 = new Vertex ("8");
-		
-		//Creation of Edges
+//		Creation of Vertices
+		Vertex vtx1 = new Vertex("1");
+		Vertex vtx2 = new Vertex("2");
+		Vertex vtx3 = new Vertex("3");
+		Vertex vtx4 = new Vertex("4");
+		Vertex vtx5 = new Vertex("5");
+		Vertex vtx6 = new Vertex("6");
+		Vertex vtx7 = new Vertex("7");
+		Vertex vtx8 = new Vertex("8");
+
+//		Creation of Edges
 		Edge edgA = new Edge(8, vtx1, vtx2);
 		Edge edgB = new Edge(4, vtx1, vtx3);
 		Edge edgC = new Edge(1, vtx3, vtx2);
@@ -31,38 +36,48 @@ public class main {
 		Edge edgJ = new Edge(3, vtx5, vtx6);
 		Edge edgK = new Edge(1, vtx7, vtx8);
 		Edge edgL = new Edge(2, vtx6, vtx8);
+
+//		Instantiation of the adjacency matrix
+		CommonKnowledge.matGraph = new MatrixGraph(8);
 		
-		//Creation of the adjacency matrix, but it's empty
-		MatrixGraph adjMtx = new MatrixGraph(8);
-		adjMtx.vertexAdd(vtx1);
-		adjMtx.vertexAdd(vtx2);
-		adjMtx.vertexAdd(vtx3);
-		adjMtx.vertexAdd(vtx4);
-		adjMtx.vertexAdd(vtx5);
-		adjMtx.vertexAdd(vtx6);
-		adjMtx.vertexAdd(vtx7);
-		adjMtx.vertexAdd(vtx8);
+//		Insertion of Vertex into adjacency matrix
+		CommonKnowledge.matGraph.vertexAdd(vtx1);
+		CommonKnowledge.matGraph.vertexAdd(vtx2);
+		CommonKnowledge.matGraph.vertexAdd(vtx3);
+		CommonKnowledge.matGraph.vertexAdd(vtx4);
+		CommonKnowledge.matGraph.vertexAdd(vtx5);
+		CommonKnowledge.matGraph.vertexAdd(vtx6);
+		CommonKnowledge.matGraph.vertexAdd(vtx7);
+		CommonKnowledge.matGraph.vertexAdd(vtx8);
+
+//		Insertion of the edges into adjacency matrix
+		CommonKnowledge.matGraph.edgeAdd(edgA);
+		CommonKnowledge.matGraph.edgeAdd(edgB);
+		CommonKnowledge.matGraph.edgeAdd(edgC);
+		CommonKnowledge.matGraph.edgeAdd(edgD);
+		CommonKnowledge.matGraph.edgeAdd(edgE);
+		CommonKnowledge.matGraph.edgeAdd(edgF);
+		CommonKnowledge.matGraph.edgeAdd(edgG);
+		CommonKnowledge.matGraph.edgeAdd(edgH);
+		CommonKnowledge.matGraph.edgeAdd(edgI);
+		CommonKnowledge.matGraph.edgeAdd(edgJ);
+		CommonKnowledge.matGraph.edgeAdd(edgK);
+		CommonKnowledge.matGraph.edgeAdd(edgL);
+
+//		Display adjacency matrix to control it
+		CommonKnowledge.matGraph.adjMatDisplay();
+
+//		Instantiation of the Ant Colony algorithm
+		AntCo antColony = new AntCo(vtx1, vtx8);
 		
-		//Insertion of the edges
-		adjMtx.edgeAdd(edgA);
-		adjMtx.edgeAdd(edgB);
-		adjMtx.edgeAdd(edgC);
-		adjMtx.edgeAdd(edgD);
-		adjMtx.edgeAdd(edgE);
-		adjMtx.edgeAdd(edgF);
-		adjMtx.edgeAdd(edgG);
-		adjMtx.edgeAdd(edgH);
-		adjMtx.edgeAdd(edgI);
-		adjMtx.edgeAdd(edgJ);
-		adjMtx.edgeAdd(edgK);
-		adjMtx.edgeAdd(edgL);
+//		Initiate all ant thread
+		antColony.initThreads();
 		
-		//Display adjacency matrix to control it
-		adjMtx.adjMatDisplay();
+//		Wait for the end of all threads
+		antColony.endThreads();
 		
-		
-		
-		
+//		All all ants to return at their starting point
+		antColony.getBack();
 	}
 
 }
