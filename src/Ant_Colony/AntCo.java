d34492650edb77bb82ca49a24b7e739e1f43f59a
ROMET <rometpierre@gmail.com>
2019-05-21 17:22:25 +0200

    Ant_Colony AntCo class to debug

--- src/Ant_Colony/AntCo.java
+++ src/Ant_Colony/AntCo.java
@@ -14,22 +14,61 @@ public class AntCo {
 
 	private ArrayList<Ant> antArray;
 
-	public AntCo(	String Starting_Vertex,
-								String Ending_Vertex,
-								Vector<Vector<Edge>> adjcencyMatrix)
+	public AntCo(	Vertex Starting_Vertex,
+								Vertex Ending_Vertex)
 	{
 		
-//	Provide a replica of the current adjacency matrix
-		CommonKnowledge.setAdjMtx_replica(adjcencyMatrix);
-		
 //	Set all ants at the same depart Vertex and provide them the same end Vertex
-		for(int i=0; i < CommonKnowledge.AdjMtxSize(); i++) {
+		for(int i=0; i < CommonKnowledge.matGraph.size(); i++) {
 			antArray.set(i, new Ant(Starting_Vertex, Ending_Vertex));
 		}
 	}
-
-	public void run() {
+	
+	/**
+	 * Initiate all ants thread
+	 */
+	public void initThreads() {
+		for (Ant ant : antArray) {
+			ant.start();
+		}
+	}
+	
+	/**
+	 * Wait for the end of all threads
+	 */
+	public void endThreads() {
+		for (Ant ant : antArray) {
+			try {
+				ant.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("thread interruption Occur");
+			}
+		}
+	}
+	
+	
+	/**
+	 * Allow to getBack ants to its starting vertex
+	 * @return
+	 */
+	public void getBack() {
 		
+		for (Ant ant : antArray) {
+			if(ant.getAntState() == state.RETURNING) {
+				for (Edge edges : ant.getTabuEdge()) {
+					edges.setLength((int)CommonKnowledge.Dorigo_evaporation(edges, ant.getDist()));
+				}
+			}
+		}		
+		System.out.println("All pheromone strength has been updated.");		
+	}
+	
+	/**
+	 * Local research function
+	 */
+	public void localSearch() {
+		//TODO
 	}
 
 }
