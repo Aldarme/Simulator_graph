e263d45fdb17e45d739039346d1772ecea2a803e
ROMET <rometpierre@gmail.com>
2019-05-21 17:23:11 +0200

    Ant_Colony CommonKnwldg class to debug

--- src/Ant_Colony/CommonKnowledge.java
+++ src/Ant_Colony/CommonKnowledge.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 import Graph.Edge;
+import Graph.MatrixGraph;
 
 /**
  * 
@@ -17,7 +18,7 @@ public class CommonKnowledge {
 	private static float evaporation;												//evaporation coefficient to update edge's strength phero.
 	private static int   optimalPathLght;										//length of the optimal tour realize by an ant
 	private static ArrayList<ArrayList<Float>> pheromones;	//adjacency pheromone matrix
-	private static Vector<Vector<Edge>> adjMtx_replica;			//replica of the current adjacency matrix
+	public 	static MatrixGraph matGraph;										//adjacency matrix
 	
 	public static void nbrCtSet (int nbr) {
 		nbrOfCities = nbr;
@@ -52,7 +53,7 @@ public class CommonKnowledge {
 	}
 	
 	/**
-	 * Allow to set pheromones strength of an edge.
+	 * Allow to set pheromone strength of an edge.
 	 * @param vtxIn
 	 * @param vtxOut
 	 * @param value
@@ -62,34 +63,11 @@ public class CommonKnowledge {
 	}
 	
 	/**
-	 * Return a distance specify between two Vertex
-	 * @return
-	 */
-	public static Integer getAdjMtx_replica(int vtxIn, int vtxOut) {
-		return adjMtx_replica.get(vtxIn).get(vtxOut).getLgh();
-	}
-	
-	/**
-	 * Realize a replica of the current adjacency matrix
-	 * @param adjMtx_p
-	 */
-	public static void setAdjMtx_replica(Vector<Vector<Edge>> adjMtx_p) {
-		for(int i=0; i < adjMtx_replica.size(); i++) {
-			adjMtx_replica.set(i, adjMtx_p.get(i));
-		}
-	}
-	
-	public static int AdjMtxSize() {
-		return adjMtx_replica.size();
-	}
-	
-	/**
 	 * Allow to evaporate (update) pheromones on the edges visited by an Ant
 	 * to construct his tour.
 	 */
-	public void evaporate() {
-//	on dépose les phéromones sur les arcs qui ont étés visités par la fourmis pour la création de son toure.
-		
+	public static float Dorigo_evaporation(Edge edge_p, int tourLenght_p) {
+		return ((1.0f-evaporation) * getPheromones(matGraph.getVtxNum(edge_p.getVtxIn()),CommonKnowledge.matGraph.getVtxNum(edge_p.getVtxOut())) + (1.0f / tourLenght_p));
 	}
 	
 }
